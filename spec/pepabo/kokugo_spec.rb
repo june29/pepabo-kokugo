80e633c7252a054c569f043c9b2749ab3000ef97
june29 <june29.jp@gmail.com>
2017-12-28 01:34:25 +0900

    :sparkles: specを整える

--- spec/pepabo/kokugo_spec.rb
+++ spec/pepabo/kokugo_spec.rb
@@ -3,12 +3,12 @@ RSpec.describe Pepabo::Kokugo do
     expect(Pepabo::Kokugo::VERSION).not_to be nil
   end
 
-  it "こんにちは" do
-    expect(Pepabo::Kokugo.convert('こんにちは')).to eq('ボプピピパ')
+  it "きちむ" do
+    expect(Pepabo::Kokugo.convert('きちむ')).to eq('ピピプ')
   end
 
-  it "やりたいことが、すぐできる。" do
-    expect(Pepabo::Kokugo.convert('やりたいことが、すぐできる。')).to eq('パピパピボボパ、ププペピプ。')
+  it "こんにちは" do
+    expect(Pepabo::Kokugo.convert('こんにちは')).to eq('ボプピピパ')
   end
 
   it "しぶやくさくらがおか" do
@@ -19,19 +19,19 @@ RSpec.describe Pepabo::Kokugo do
     expect(Pepabo::Kokugo.convert('かもがわシーワールド')).to eq('パボパパピーパープボ')
   end
 
-  it "きちむ" do
-    expect(Pepabo::Kokugo.convert('きちむ')).to eq('ピピプ')
+  it "やりたいことが、すぐできる。" do
+    expect(Pepabo::Kokugo.convert('やりたいことが、すぐできる。')).to eq('パピパピボボパ、ププペピプ。')
   end
 
   it "いつでもどこでも楽しめる。" do
-    expect(Pepabo::Kokugo.convert('いつでもどこでも楽しめる。')).to eq('')
+    expect(Pepabo::Kokugo.convert('いつでもどこでも楽しめる。')).to eq('ピプペボボボペボパボピペプ。')
   end
 
   it "いのちの輝きと、ふれあえる海。" do
-    expect(Pepabo::Kokugo.convert('いのちの輝きと、ふれあえる海。')).to eq('')
+    expect(Pepabo::Kokugo.convert('いのちの輝きと、ふれあえる海。')).to eq('ピボピボパパパピボ、プペパペププピ。')
   end
 
   it "明るく、にこにこ、大きな声。" do
-    expect(Pepabo::Kokugo.convert('明るく、にこにこ、大きな声。')).to eq('')
+    expect(Pepabo::Kokugo.convert('明るく、にこにこ、大きな声。')).to eq('パパププ、ピポピポ、ボボピパボペ。')
   end
 end
