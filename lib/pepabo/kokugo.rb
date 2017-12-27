5fedbc277d7634bc720df119a810cea18c991310
june29 <june29.jp@gmail.com>
2017-12-28 01:55:07 +0900

    :sparkles: 「きちむ」に対応する by @bake0937

--- lib/pepabo/kokugo.rb
+++ lib/pepabo/kokugo.rb
@@ -2,10 +2,22 @@ module Pepabo
   module Kokugo
     VERSION = '0.1.0'
 
+    CONVERT_RULES = {
+      'き' => 'ピ',
+      'ち' => 'ピ',
+      'む' => 'プ',
+    }
+
     module_function
 
     def convert(string)
-      string
+      val = ''
+
+      string.chars.each { |char|
+        val += CONVERT_RULES[char]
+      }
+
+      val
     end
   end
 end
