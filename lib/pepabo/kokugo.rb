7600e90afaaf30a2123a1655f59b5a313da021cf
june29 <june29.jp@gmail.com>
2017-12-28 01:31:43 +0900

    :sparkles: 一通りを用意する

---
+++ lib/pepabo/kokugo.rb
@@ -0,0 +1,11 @@
+module Pepabo
+  module Kokugo
+    VERSION = '0.1.0'
+
+    module_function
+
+    def convert(string)
+      string
+    end
+  end
+end
---
+++ spec/pepabo/kokugo_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe Pepabo::Kokugo do
+  it "has a version number" do
+    expect(Pepabo::Kokugo::VERSION).not_to be nil
+  end
+
+  it "こんにちは" do
+    expect(Pepabo::Kokugo.convert('こんにちは')).to eq('ボプピピパ')
+  end
+
+  it "やりたいことが、すぐできる。" do
+    expect(Pepabo::Kokugo.convert('やりたいことが、すぐできる。')).to eq('パピパピボボパ、ププペピプ。')
+  end
+
+  it "しぶやくさくらがおか" do
+    expect(Pepabo::Kokugo.convert('しぶやくさくらがおか')).to eq('ピプパプパプパパボパ')
+  end
+
+  it "かもがわシーワールド" do
+    expect(Pepabo::Kokugo.convert('かもがわシーワールド')).to eq('パボパパピーパープボ')
+  end
+
+  it "きちむ" do
+    expect(Pepabo::Kokugo.convert('きちむ')).to eq('ピピプ')
+  end
+
+  it "いつでもどこでも楽しめる。" do
+    expect(Pepabo::Kokugo.convert('いつでもどこでも楽しめる。')).to eq('')
+  end
+
+  it "いのちの輝きと、ふれあえる海。" do
+    expect(Pepabo::Kokugo.convert('いのちの輝きと、ふれあえる海。')).to eq('')
+  end
+
+  it "明るく、にこにこ、大きな声。" do
+    expect(Pepabo::Kokugo.convert('明るく、にこにこ、大きな声。')).to eq('')
+  end
+end
